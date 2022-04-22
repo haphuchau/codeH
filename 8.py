a76f95dab82ec066e9b879c1ac4a82c932acc10f
Hà Phúc Hậu <45466178+haphuchau@users.noreply.github.com>
2022-04-23 00:01:32 +0700

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,17 @@
+CURRENT_YEAR = 2022
+METER_TO_FEET = 3.281	
+
+firstname = input("Your fristname: ")
+lastname = input("Your lastname: ")
+year_born = int(input("When you were born: "))
+height_meter = float(input("Your height (meter): "))
+
+age = CURRENT_YEAR - year_born
+
+height_feet = height_meter * METER_TO_FEET
+height_feet = round(height_feet,1)
+
+print("\n-----")
+print( "Your are " + str(height_feet) + " feet tall " )
+print("{2} is {0} year old in {1}".format(age,CURRENT_YEAR,firstname))
+print("Your name is " + firstname + " " + lastname )
